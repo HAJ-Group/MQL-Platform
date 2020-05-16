ac53ba8fe19bb20e1bf5042471f0d3ddcb3566a2
Ennouinou Alae <alaeddinne.ennouinou@gmail.com>
2020-05-16 10:13:23 +0000

    Program is now dynamic

--- components/HomeComponent/js/HomeComponent.js
+++ components/HomeComponent/js/HomeComponent.js
@@ -1,14 +1,34 @@
 /*Global Variables*/ 
 let view; 
-let service; 
+let service;
 /*Default class*/ 
 function HomeComponent(service) { 
 	//TODO: Intitialize controller for HomeComponent
 	loadResources();
 	this.service = service;
+	this.table=this.get("table-program");
 } 
+HomeComponent.prototype.get = function (id) {
+	return document.getElementById(id);
+}
+HomeComponent.prototype.addColumn=function (program) {
+	var row=this.table.insertRow();
+	var cell=row.insertCell();
+	cell.innerHTML += "<span class='semester'> Semestre "+program.id +" </span>"+"<hr>"+"<ul>";
+	for (let i = 0; i < program.modules.length ; i++) {
+		cell.innerHTML+="<li>"+"M"+(i+1)+" : "+program.modules[i]+"</li>";
+	}
+	cell.innerHTML += "</ul>" +" <br>";
+}
+HomeComponent.prototype.printSemesters=function () {
+	for (let i = 0; i < this.service.size(); i++) {
+		this.addColumn(this.service.get(i));
+	}
+}
 /* Main Function */ 
 function main() {
-	service = new HomeComponentService(); 
+	service = new HomeComponentService();
+	service.load(db);
 	view = new HomeComponent(service);
+	view.printSemesters();
 } 
