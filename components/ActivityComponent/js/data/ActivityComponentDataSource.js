a53ccec68a845ede4e5f5c7ec67750ca22dff998
jaouad <jaouadaitassou@gmail.com>
2020-05-19 16:20:38 +0000

    Modifying the data structure

--- components/ActivityComponent/js/data/ActivityComponentDataSource.js
+++ components/ActivityComponent/js/data/ActivityComponentDataSource.js
@@ -1,24 +1,29 @@
 /* Global data source table for ActivityComponent */ 
 let dbActivity = [ 
 	// Add object structure associated with the model created 
-	{ 
-		'cours' : [
-			['cours-1', 'lurem ipsum lurem ipsum'],
-			['cours-2', 'lurem ipsum lurem ipsum']
+	{
+		'subject' : [
+			{
+				name: 'cours1',
+				d: [
+					['cours-1', 'lurem ipsum lurem ipsum'],
+					['cours-2', 'lurem ipsum lurem ipsum']
+				]
+			}
 		]
-
 	}
+
 	/*,
 	{
-		'traveaux-equipe' : [
-			['travEq-1', 'lurem ipsum lurem ipsum'],
-			['travEq-2', 'lurem ipsum lurem ipsum']
+		'subject' : [
+			{
+				name: 'travail',
+				d: [
+					['travail-12', 'lurem ipsum lurem ipsum'],
+					['travail-22', 'lurem ipsum lurem ipsum']
+				]
+			}
 		]
-	},
-	{
-		'travail-indiv' : [
-			['travIndiv-1', 'lurem ipsum lurem ipsum'],
-			['travIndiv-2', 'lurem ipsum lurem ipsum']
-		]
-	}*/
+	},*/
+
 ]; 
