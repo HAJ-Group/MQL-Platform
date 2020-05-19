827161b04d39d654b60024d858cd4dec0b73a80e
hamza alawi <hamza_hm@hotmail.be>
2020-05-19 04:40:21 +0000

    adding page number algorithm

--- components/NewsComponent/js/NewsComponent.js
+++ components/NewsComponent/js/NewsComponent.js
@@ -2,15 +2,22 @@
 let view; 
 let service;
 let current_page_number = 1;
+const MAX_PAGES_PER_PAGE = 5;
 /*Default class*/ 
 function NewsComponent(service) { 
 	current_component = 'News';
 	loadResources(); 
 	this.service = service;
 	// this.table = this.get('table-NewsID');
+	this.page_blocks = split(this.service.db, MAX_PAGES_PER_PAGE);
 	this.block_nav = this.get('navigation');
 	this.block_main = this.get('main');
 	this.block_switch = this.get('switcher');
+	this.htmlSaver = {
+		nav: this.block_nav.innerHTML,
+		main: this.block_main.innerHTML,
+		switcher: this.block_switch.innerHTML,
+	};
 }
 NewsComponent.prototype.get = function (id) {
 	return document.getElementById(id);
@@ -26,18 +33,24 @@ NewsComponent.prototype.printNewsList = function (max = this.service.size()) {
 	}
 };
 
+/**
+ * Create navigation menu dynamically
+ */
 NewsComponent.prototype.fillNavigation = function () {
-	let htmlContent = this.block_nav.innerHTML;
-	for(let news of this.service.db) {
+	let htmlContent = this.htmlSaver.nav;
+	for(let news of this.page_blocks[current_page_number - 1]) {
 		htmlContent += '<hr>\n' +
 			'<div><a class="menuitem" href="#' + news.id + '">' + news.title + '</a></div>\n';
 	}
 	this.block_nav.innerHTML = htmlContent;
 };
 
+/**
+ * Create main block dynamically
+ */
 NewsComponent.prototype.fillMain = function () {
-	let htmlContent = this.block_main.innerHTML;
-	for(let news of this.service.db) {
+	let htmlContent = this.htmlSaver.main;
+	for(let news of this.page_blocks[current_page_number - 1]) {
 		htmlContent += '<div id="' + news.id + '"">\n' +
 			'<div class="title">\n' +
 			news.title +
@@ -51,14 +64,28 @@ NewsComponent.prototype.fillMain = function () {
 	this.block_main.innerHTML = htmlContent;
 };
 
+/**
+ * Create page switcher dynamically
+ */
 NewsComponent.prototype.fillSwitcher = function () {
-	let htmlContent = this.block_switch.innerHTML;
-	let pages = split(this.service.db, 10).length;
-	for(let i = 0; i<pages; i++) {
-		if(current_page_number === i) htmlContent += '<span class="active-page"><a>' + (i+1) + '</a></span>';
-		else htmlContent += '<span><a>' + (i+1) + '</a></span>';
+	let htmlContent = this.htmlSaver.switcher;
+	let pages = this.page_blocks.length;
+	for(let i = 1; i<=pages; i++) {
+		if(current_page_number === i) htmlContent += '<span id="linker_' + i + '" onclick="view.navigate(' + i + ')" class="active-page">' + i + '</span>';
+		else htmlContent += '<span id="linker_' + i + '" onclick="view.navigate(' + i + ')">' + i + '</span>';
 	}
 	this.block_switch.innerHTML = htmlContent;
+};
+
+/**
+ * Navigate between pages
+ * @param page_number
+ */
+NewsComponent.prototype.navigate = function(page_number) {
+	current_page_number = page_number;
+	this.fillNavigation();
+	this.fillMain();
+	this.fillSwitcher();
 };
 
 /* Main Function */ 
@@ -70,5 +97,6 @@ function main() {
 	view.fillNavigation();
 	view.fillMain();
 	view.fillSwitcher();
+	view.navigate(current_page_number);
 	addTitleIcon('../../resources/pictures/News-logo.png');
 } 
