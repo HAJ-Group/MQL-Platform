ed96c33b53664936be5d9845cf4d624c7399a624
Ennouinou Alae <alaeddinne.ennouinou@gmail.com>
2020-05-28 10:51:30 +0000

    Modify content in home, Event and Activity

--- components/ActivityComponent/js/data/ActivityComponentDataSource.js
+++ components/ActivityComponent/js/data/ActivityComponentDataSource.js
@@ -4,15 +4,15 @@ let dbActivity = [
 
 	{
 		id: 1,
-		description: ['../../resources/pictures/cafe.png', 'Le premier semestre constitue une formation de base importante pour l\'étudaint, dans laquelle ce dernier acquéris un baguage théorique et pratique à travers un ensemble de sous-formations (modules) techniques et de communication. Dans ce semestre chaque étudiant aura des fortes connaissances techniques individuelles.'],
+		description: ['../../resources/pictures/cafe.png', 'Le premier semestre constitue une formation de base importante pour l\'étudiant, dans lequel ce dernier acquis un bagage théorique et pratique à travers un ensemble de sous-formations (modules) techniques (LES BONNES PRATIQUES), d\'autres visent la communication et le développement des SoftSkills grâce à des présentations ou des travaux d\'équipes (les étudiants sont amenés de travailler en grouep afin de réaliser un projet web et mobile(en s2) en se basant sur les principes de la gestion des projets et les méthodes agiles) . L\'objectif est que chaque étudiant aie des fortes connaissances techniques individuelles avec un développement sur le côté Softskills.'],
 		modules: [
 		        '../../resources/pictures/Activities/modules3.jpg',
 				'Ingénierie de l\'objet et thechnologie Java',
 				'Bases de données ORACLE',
 				'Technologie XML',
 				'Téchniques d\'éxpressions et communication',
-				'Géstion de projets',
-				'Ingénierie logiciélle et UML'
+				'Géstion de projets et qualité du logiciel',
+				'Ingénierie logicielle et UML'
 				],
 		activity: ['../../resources/pictures/Activities/values.png',
 			'Compétences Techniques et technologiques (Chaque module est une formation théorique et technique) ',
@@ -24,7 +24,7 @@ let dbActivity = [
 	},
 	{
 		id: 2,
-		description:  ['../../resources/pictures/cafe.png', 'Le deuxième semestre se concentre dans une grande partie sur le travaille d\'équipes des étudiants. Ainsi que la métrise de l\'art de la communication à travers des présentations du groupe chaque semaine, et l\'acquésition des différentes méthodologies de travail et des méthodes Agiles notammant SCRUM.'],
+		description:  ['../../resources/pictures/cafe.png', 'Le deuxième semestre garantie un développement pour l\'étudiant sur le côté technique en avancant sur plusieurs aspects informatiques notamment les design patterns,WEB,JAVA EE,... et sur le côté de la communication sachant qu\'une grande partie est consacrée pour développer l\'esprit d\'équipes chez les étudiants qui sont amenés à présenter chaque semaine un sujet informatique ( Business Intelligence, Framework JAVA EE, EJB ,DotNet, les outils d\'industrialisation,JavaScript API et Frameworks) pour une durée allant de 15 à 30 minutes sous le principe d\'être concis et précis.'],
 		modules: [
             '../../resources/pictures/Activities/modules3.jpg',
             'Design Patterns',
@@ -45,7 +45,7 @@ let dbActivity = [
 	},
 	{
 		id: 3,
-		description:  ['../../resources/pictures/cafe.png', 'Le troixième semestre permet de renforcer les connaissances techniques et de communication des étudiants, Ainsi que la bonne connaissance de l\'entreprise et la maitrise de ces différents métiers.'],
+		description:  ['../../resources/pictures/cafe.png', 'Le troisième semestre permet de renforcer les connaissances techniques et de communication des étudiants, ainsi que la bonne connaissance de l\'entreprise et la maitrise de ces différents métiers vu qu\'ils sont sur le point de commencer leurs stages pré-embauche. Un étudiant MQL est alors prêt pour passer des entretiens de stages des différents multinationales'],
 		modules: [
             '../../resources/pictures/Activities/modules3.jpg',
             'Frameworks Java EE',
