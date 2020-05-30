46047f52f2b90b00f3ea508b4731d7611b84d8d4
Ennouinou Alae <alaeddinne.ennouinou@gmail.com>
2020-05-30 17:29:05 +0000

    We can add,edit and modify partners

--- components/PartnerComponent/js/PartnerComponent.js
+++ components/PartnerComponent/js/PartnerComponent.js
@@ -1,15 +1,18 @@
 /*Global Variables*/ 
 let view; 
-let service; 
-/*Default class*/ 
+let service;
+// initaliser first blocks :
+let menu=$('#partners-menu');
+let container=$('#partners-container');
+/*Default class*/
 function PartnerComponent(service) { 
 	//TODO: Intitialize controller for PartnerComponent 
 	current_component = 'Partner'; 
 	loadResources(); 
 	this.service = service; 
 	//this.table = $('#table-PartnerID'); Uncomment for apply dynamic data loading to a declared html tag by id (Add other tables if needed with associated methods)
-	this.block = $('#container');
-	this.partners = $('#partners');
+	this.block = $('#partners-container');
+	this.partners = $('#partners-menu');
 	this.currentblock=null;
 }
 
@@ -56,8 +59,6 @@ PartnerComponent.prototype.printPartners = function () {
 		this.addOnePartner(this.service.get(i));
 	}
 };
-
-
 PartnerComponent.prototype.show = function (id) {
     let hide_block= $('#' + this.currentblock);
 	$('#menu-' + this.currentblock).classList.remove('active');
@@ -69,12 +70,13 @@ PartnerComponent.prototype.show = function (id) {
 };
 PartnerComponent.prototype.show2 = function (id) {
 	view.show(id);
-	window.location.href='#'+id;
+	route('#'+id);
 };
 PartnerComponent.prototype.hideAll = function () {
 	for (let i = 1; i < this.service.size(); i++) {
 		let partner = $('.card')[i];
 		partner.style['display'] = 'none';
+		if($('.partner')[i]!==undefined)
 		$('.partner')[i].classList.remove('active');
 	}
 };
@@ -83,7 +85,7 @@ PartnerComponent.prototype.trigger = function () {
 	let anchor = window.location.href.split('#')[1];
 	if(anchor !== undefined) {
 		$('#menu-' + anchor).click();
-		window.location.href = '#' + anchor;
+		route('#' + anchor);
 	}
 };
 
@@ -92,6 +94,98 @@ PartnerComponent.prototype.ajustLinks = function () {
 	for(let link of links) {
 		link.setAttribute('onclick', 'view.show2(\'' + link.id + '\')');
 	}
+};
+/* FORM SERVICES */
+PartnerComponent.prototype.addData = function() {
+	$('#partnerSubmit').setAttribute('onclick', 'view.submitData()');
+	popFORM();
+};
+
+PartnerComponent.prototype.editData = function(index) {
+	let el_name = $('#partnerName');
+	let el_color = $('#partnerColor');
+	let el_ca=$('#partnerCa');
+	let el_desc = $('#partnerDescription');
+	let el_co=$('#partnerCo');
+	let el_website=$('#partnerWebSite');
+
+	//....
+	let target = this.service.get(index);
+	el_name.value = target.name;
+	el_color.value= target.color;
+	el_ca.value= target.ca;
+	el_desc.value = target.description;
+	el_co.value = target.nbr_colla;
+	el_website.value = target.website;
+	//...
+	$('#partnerSubmit').setAttribute('onclick', 'view.submitData(\'edit\', ' + index + ')');
+	popFORM();
+};
+
+PartnerComponent.prototype.deleteData = function(index) {
+	if(confirm('Are you sure you want to delete this Partner ?')) {
+		this.service.remove(index);
+		//....
+		this.navigate();
+	}
+};
+
+PartnerComponent.prototype.submitData = function (action = 'add', index = '0') {
+	// GETTING DATA MEMBERS
+	let name = $('#partnerName').value;
+	let color = $('#partnerColor').value;
+	let ca=$('#partnerCa').value;
+	let desc = $('#partnerDescription').value;
+	let co=$('#partnerCo').value;
+	let website=$('#partnerWebSite').value;
+	//...
+	if(action === 'add') {
+		this.service.add(new Partner(this.service.size() + 1,'',name,ca,desc,co,[],website,''));
+	}
+	if(action === 'edit') {
+		let target = this.service.get(index);
+		target.name = name;
+		target.color = color;
+		target.ca = ca;
+		target.description = desc;
+		target.nbr_colla = co;
+		target.website = website;
+		//...
+		$('#partnerSubmit').setAttribute('onclick', 'view.submitData()');
+	}
+	closeFORM();
+	this.navigate();
+};
+PartnerComponent.prototype.triggerSubmit = function () {
+	let submit_element = $('#partnerSubmit');
+	submit_element.click();
+};
+PartnerComponent.prototype.addButtons = function() {
+	let names = $('.title');
+	let i=0;
+	for (let name of names) {
+		if(localStorage.getItem('ACCESS') !== 'null') {
+			// ADD EDIT AND DELETE ICONS
+			name.innerHTML += '<img name="edit-icon" src="../../resources/pictures/icons/edit.png" alt=""  ' +
+				'class="sh-icon" onclick="view.editData(' + i + ')">' +
+				'<img name="delete-icon" src="../../resources/pictures/icons/delete.png" alt=""  ' +
+				'class="sh-icon" onclick="view.deleteData(' + i + ')">';
+		}
+		i++;
+	}
+	if(localStorage.getItem('ACCESS') !== 'null') {
+		// ADD NEW ICON BLOCK
+		let saver = $('.sub-content')[0];
+		saver.innerHTML = '<div class="new-block"><img onclick="view.addData()" src="../../resources/pictures/icons/new-icon.png" alt="" class="new-icon"></div>' ;
+	}
+};
+PartnerComponent.prototype.navigate = function() {
+	view.printPartners();
+	view.hideAll();
+	view.trigger();
+	view.ajustLinks();
+	view.addButtons();
+	setKeysAction('.form-content',view.triggerSubmit.bind(view));
 };
 /**-------------------------------------------------------------------------------------------------------------------*/
 /* Main Function */ 
@@ -104,4 +198,6 @@ function main() {
 	view.hideAll();
 	view.trigger();
 	view.ajustLinks();
-} 
+	view.addButtons();
+	setKeysAction('.form-content',view.triggerSubmit.bind(view));
+}
